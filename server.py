a44054beeff7e7448e9ca052e40e184797e3895e
w1gg3r <gold.dendi@mail.ru>
2025-05-28 10:02:46 +0300

    patp

--- server.py
+++ server.py
@@ -291,4 +291,6 @@ def page_not_found(e):
 
 if __name__ == '__main__':
     init_db()
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))  # Используй PORT от Render или 5000 локально
+    app.run(host='0.0.0.0', port=port, debug=False)
